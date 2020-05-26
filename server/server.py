ca1a4d17954c08aeb04c6a6db7d16ba47254a62a
Diromer337 <58221682+Diromer337@users.noreply.github.com>
2020-05-26 14:39:53 +0300

    Add files via upload

--- server/server.py
+++ server/server.py
@@ -1,16 +1,29 @@
 import redis
 from flask import Flask
 from flask import abort
+from flask import make_response
+from flask import render_template
 from flask import request
+
 from user import User
 
 app = Flask(__name__)
+
 log_history = redis.Redis(host='redis', port=6379)
+
+
+@app.errorhandler(429)
+def error(e):
+    resp = make_response(render_template('429.html'), 429)
+    resp.headers['Retry-After'] = str(e.description)
+    return resp
 
 
 @app.route('/reset')
 def reset():
-    pass
+    user = User(request, log_history)
+    user.limit_reset()
+    return 'reseted'
 
 
 @app.route('/')
@@ -19,8 +32,8 @@ def start():
     if user.not_banned():
         return 'OK'
     else:
-        abort(429, 'You only allow 100 requests')
+        abort(429, description=user.get_ban_time())
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 1337)
+    app.run(host='0.0.0.0', debug=True, port=1337)
--- server/user.py
+++ server/user.py
@@ -1,5 +1,6 @@
 import configparser
 import time
+import ipaddress
 
 import redis
 from flask import request
@@ -7,28 +8,39 @@ from flask import request
 
 class User:
     def __init__(self, user_request: request, db: redis.Redis):
-        self.user_request = user_request
         self.db = db
-        self.max_req, self.ban_time = self.read_config()
+        self.max_req, self.ban_time, self.prefix_size = self._read_config()
+        self.user_network = self._get_network(user_request)
+
+    def get_ban_time(self):
+        return self.ban_time
 
     @staticmethod
-    def read_config():
+    def _read_config():
         config = configparser.ConfigParser()
         config.read('config.ini')
-        max_req = config['DEFAULT']['max_req']
-        ban_time = config['DEFAULT']['max_req']
-        return max_req, ban_time
+        max_req = int(config['DEFAULT']['max_req'])
+        ban_time = int(config['DEFAULT']['ban_time'])
+        prefix_size = config['DEFAULT']['prefix_size']
+        return max_req, ban_time, prefix_size
+
+    def _get_network(self, user_request: request):
+        if user_request.headers.getlist('X-Forwarded-For'):
+            ip = user_request.headers.getlist('X-Forwarded-For')[0]
+        else:
+            ip = user_request.remote_addr
+        user_network = ipaddress.ip_interface(ip + '/' + self.prefix_size)
+        return str(user_network.network)
 
     def not_banned(self):
-        subnet = self.get_subnet(self.user_request)
         request_time = int(time.time())
-        if self._check_ban(subnet, request_time):
+        if self._check_ban(self.user_network, request_time):
             return False
-        self._del_old_requests(subnet, request_time)
-        self.db.rpush(subnet, str(request_time))
-        if self.db.llen(subnet) == 100:
-            self.db.delete(subnet)
-            self.db.rpush(subnet, 'ban', str(request_time))
+        self._del_old_requests(self.user_network, request_time)
+        self.db.rpush(self.user_network, str(request_time))
+        if self.db.llen(self.user_network) == self.max_req:
+            self.db.delete(self.user_network)
+            self.db.rpush(self.user_network, 'ban', str(request_time))
         return True
 
     def _del_old_requests(self, subnet: str, request_time: int):
@@ -39,22 +51,15 @@ class User:
             if len(requests) != 0:
                 self.db.rpush(subnet, *requests)
 
-    @staticmethod
-    def get_subnet(user_request):
-        if user_request.headers.getlist('X-Forwarded-For'):
-            ip = user_request.headers.getlist('X-Forwarded-For')[0]
-        else:
-            ip = user_request.remote_addr
-        lst = ip.split('.')
-        subnet = '.'.join(lst[0:-1])
-        return subnet
+    def limit_reset(self):
+        self.db.delete(self.user_network)
 
     def _check_ban(self, subnet: str, request_time: int) -> bool:
         if self.db.llen(subnet) != 0:
             status = self.db.lrange(subnet, 0, 0)[0]
             if status == b'ban':
                 ban_time = int(self.db.lrange(subnet, 1, 1)[0])
-                if request_time - ban_time > 120:
+                if request_time - ban_time > self.ban_time:
                     self.db.delete(subnet)
                     return False
                 else:
